be60202f6c44a870a967945a8cbefcf554f95dbc
Daniel Halterman <olienad@yahoo.com>
2025-09-27 11:40:22 -0400

    Changed header to use include guard and made size an int

--- include/lexer.h
+++ include/lexer.h
@@ -1,17 +1,17 @@
-#pragma once
-
-#include <stdlib.h>
-#include <stdbool.h>
+#ifndef LEXER_H
+#define LEXER_H
 
 typedef struct {
-	char** items;
-	size_t size;
+    char** items;
+    int size;
 } tokenlist;
 
-char * get_input(void);
-tokenlist * get_tokens(char *input);
-tokenlist * new_tokenlist(void);
-void add_token(tokenlist *tokens, char *item);
+char* get_input(void);
+tokenlist* new_tokenlist(void);
+void add_token(tokenlist* tokens, char* item);
+tokenlist* get_tokens(char* input);
 void replace_token(tokenlist* tokens, int i, char* item);
 void remove_token(tokenlist* tokens, int i);
-void free_tokens(tokenlist *tokens);
+void free_tokens(tokenlist* tokens);
+
+#endif
